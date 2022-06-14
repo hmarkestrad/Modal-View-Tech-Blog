b50b239a2ded7dea956f299e076958ec1fd28e68
Mark Estrada <h.mark.estrada@gmail.com>
2022-06-14 18:36:18 -0500

    heroku testing

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 // create connection to our db
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-  host: 'localhost',
+  host: 'http://us-cdbr-east-05.cleardb.net/',
   dialect: 'mysql'
 });
 
