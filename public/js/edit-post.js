3d2c0bf996c8e20e301871c6bc413249c2ab7ed5
hmrkestrad <h.mark.estrada@gmail.com>
2022-06-14 13:54:05 -0500

    Heroku Testing

---
+++ public/js/edit-post.js
@@ -0,0 +1,31 @@
+async function editPostHandler(event) {
+    event.preventDefault();
+
+    const id = window.location.toString().split("/")[
+        window.location.toString().split("/").length - 1
+    ];
+
+    const title = document.getElementById("post-title").value.trim();
+    const content = document.querySelector('.post-content').value;
+
+    console.log(title, " ", content)
+
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            title,
+            content
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector(".edit-post-form").addEventListener("submit", editPostHandler);
