352167146129e6ff38069d7dc04e7b7ead41d484
hmrkestrad <h.mark.estrada@gmail.com>
2022-06-14 14:10:32 -0500

    Heroku Testing

--- server.js
+++ server.js
@@ -2,12 +2,9 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
-
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 const session = require('express-session');
-
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
