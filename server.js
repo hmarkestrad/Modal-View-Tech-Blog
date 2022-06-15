af5faaf6a5b2f1698a5218a75f44d5deae59ff1e
Mark Estrada <h.mark.estrada@gmail.com>
2022-06-14 20:11:01 -0500

    Heroku Testing

--- server.js
+++ server.js
@@ -2,40 +2,45 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
-const app = express();
-const PORT = process.env.PORT || 3000;
+
+const helpers = require('./utils/helpers');
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+
 const session = require('express-session');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
+  secret: 'bigbluedog',
+  cookie: {
+        // Session will automatically expire in 10 minutes
+        expires: 10 * 60 * 1000
+  },
+  resave: true,
+  rolling: true,
   saveUninitialized: true,
   store: new SequelizeStore({
     db: sequelize
-  })
+  }),
 };
 
 app.use(session(sess));
-
-//Handlebars
-const exphbs = require('express-handlebars');
-// Helpers
-const helpers = require('./utils/helpers');
-const hbs = exphbs.create({ helpers });
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// turn on routes
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(process.env.PORT || 3000, '0.0.0.0', () => console.log(`App listening on Port: http://localhost:${PORT}`));
+  app.listen(PORT, () => console.log('Now listening'));
 });
