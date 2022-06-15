fd4a6e132ae3463f68541e4dc1df4a97bf6d51c6
Mark Estrada <h.mark.estrada@gmail.com>
2022-06-14 19:31:24 -0500

    heroku testing

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,10 @@
+import Sequelize from "sequelize";
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  const sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  const sequelize = new Sequelize(
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PW,
