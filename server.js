51f2541c08d7f283e50eb332067ac1f838b16285
hmrkestrad <h.mark.estrada@gmail.com>
2022-06-20 20:48:27 -0500

    Update server.js
    Updated server.js formating

--- server.js
+++ server.js
@@ -2,45 +2,28 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
-
 const helpers = require('./utils/helpers');
-
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
-
 const session = require('express-session');
-
 const app = express();
 const PORT = process.env.PORT || 3030;
-
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const sess = {
   secret: 'bigbluedog',
-  cookie: {
-        // Session will automatically expire in 10 minutes
-        expires: 10 * 60 * 1000
-  },
+  cookie: { expires: 20 * 60 * 1000 },
   resave: true,
   rolling: true,
   saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  }),
-};
+  store: new SequelizeStore({ db: sequelize }), };
 
 app.use(session(sess));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
 app.use(routes);
 
-// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Server available at http://localhost:${PORT}`));
-});
+app.listen(PORT, () => console.log(`Server available at http://localhost:${PORT}`)); });
